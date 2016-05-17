95c926341f23f85c759a97a0cd91e42b67a6387e
陈梓麟 <chenzl25@mail2.sysu.edu.cn>
2016-05-18 01:00:46 +0800

    parse regular expression to a parse tree

---
+++ re_parse.cpp
@@ -0,0 +1,189 @@
+#include <iostream>
+#include <string>
+using namespace std;
+//*********************************
+// regular expression like that
+// (), |, `concatenate`, , *, .
+// '.' stand for arbitrary symbol
+//*********************************
+
+// #define DEGUG
+#define EOF -1
+enum TYPE {
+  TYPE_CAT,
+  TYPE_STAR,
+  TYPE_OR,
+  TYPE_CHAR
+};
+
+static string input_string; // the global input_string
+static int cur_pos;         // current position of input_string  
+static char cur_char;       // input_string[cur_pos]
+
+struct TreeNode { // the Node of the re parse tree
+  int type;
+  char ch;
+  TreeNode* left; // the TYPE_STAR use the left node only
+  TreeNode* right;
+  TreeNode(int t, char c, TreeNode* l, TreeNode* r) {
+    type = t;
+    ch = c;
+    left = l;
+    right = r;
+  }
+};
+TreeNode* parse_char();
+TreeNode* parse_or();
+TreeNode* parse_cat();
+TreeNode* parse_star();
+TreeNode* parse_re();
+TreeNode* parse(string s);
+void init();
+void move();
+void match(char t);
+void visit(TreeNode* root);
+void debug(string s);
+
+
+int main(int argc, char**argv) {
+  TreeNode* root = parse("((ab|cd|e)*(hg)*)*");
+  visit(root);
+}
+
+// the api for the other files
+TreeNode* parse(string s) {
+  input_string = s;
+  init();
+  return parse_re();
+}
+
+
+//  Grammer
+//  re   -> or
+//  or   -> or '|' cat | cat
+//  cat  -> cat star   | star
+//  star -> char '*'   | char
+//  char -> '(' re ')' | '.' | 'a' | ..... | 'z' | .....(other characters)
+
+// Invariant:
+// 1.before to parse we let cur_pos point to the start position
+// 2.after the parse we let cur_pos point to the last after the end position 
+
+
+TreeNode* parse_re() {
+  debug("parse_re");
+  return parse_or();
+}
+TreeNode* parse_or() {
+  debug("parse_or");
+  TreeNode* root = parse_cat();
+  while (cur_char == '|') {
+    move();
+    root = new TreeNode(TYPE_OR, NULL, root, parse_cat());
+  }
+  return root;
+}
+TreeNode* parse_cat() {
+  debug("parse_cat");
+  TreeNode* root = parse_star();
+  while (cur_char != EOF &&
+         cur_char != '*' &&
+         cur_char != ')' &&
+         cur_char != '|') {
+    root = new TreeNode(TYPE_CAT, NULL, root, parse_star());
+  }
+  return root;
+}
+
+TreeNode* parse_star() {
+  debug("parse_star");
+  TreeNode* root = parse_char();
+  if (cur_char == '*') {
+    move();
+    root = new TreeNode(TYPE_STAR, NULL, root, NULL);
+  }
+  return root;
+}
+
+TreeNode* parse_char() {
+  debug("parse_char");
+  TreeNode* root = NULL;
+  switch (cur_char) {
+    case '(' :
+      move();
+      root = parse_re();
+      match(')');
+      break;
+    default:
+      root = new TreeNode(TYPE_CHAR, cur_char, NULL, NULL);
+      move();
+  }
+  return root;
+}
+
+
+
+// move the cur_pos
+void move() {
+  cur_pos++;
+  if (cur_pos < input_string.length()) cur_char = input_string[cur_pos];
+  else cur_char = EOF;
+}
+// assert to match the character
+void match(char t) {
+  if (cur_char != t) {
+    cout << "syntax error, we expect " << t << endl;
+    exit(-1);
+  }
+  move();
+}
+
+void visit(TreeNode* root) {
+  if (root) {
+    visit(root->left);
+    visit(root->right);
+    switch (root->type) {
+      case TYPE_CAT:
+        cout << " -";
+        break;
+      case TYPE_OR:
+        cout << " |";
+        break;
+      case TYPE_STAR:
+        cout << " *";
+        break;
+      case TYPE_CHAR:
+        cout << " "  << root->ch;
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+void init() {
+  // here we avoid the repetition of the * and spacelike character
+  for (int i = 0; i < input_string.length(); i++) {
+    if (input_string[i] == '*' && 
+        i+1 < input_string.length() && 
+        input_string[i+1] == '*') {
+      cout << "repetition-operator *" <<endl;
+      exit(-1);
+    }
+    if (input_string[i] == ' ' || 
+        input_string[i] == '\t'||
+        input_string[i] == '\n') {
+      cout << "the regular expression should not contain the spacelike character" << endl;
+      exit(-1);
+    }
+  }
+  // prepare to parse. Before to parse we let the cur_pos point to the start position
+  cur_pos = -1;
+  move();
+}
+
+void debug(string s) {
+#ifdef DEGUG
+  cout << s << endl;
+#endif
+}
